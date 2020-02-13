e8346e200738f1d47f1dade393c8e562ac44e66e
Ludvig Ellevold <21200123+ljellevo@users.noreply.github.com>
2020-02-13 19:22:29 +0100

    Fixed ports

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.get('/*', function (req, res) {
   res.sendFile(path.resolve(__dirname, '../bin/index.html'))
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Listening on port 3000!")
 });
 
