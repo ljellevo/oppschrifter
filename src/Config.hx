063aacb4e41c54f086a7db3de602026875e53e18
Ludvig Ellevold <21200123+ljellevo@users.noreply.github.com>
2020-02-13 15:41:04 +0100

    Opdated config

--- src/Config.hx
+++ src/Config.hx
@@ -3,7 +3,7 @@
 class Config {
   
   public static final API_AUTH: String = "https://oppschrifter.herokuapp.com/api/auth";
-  public static final API_REGISTER: String = "http://localhost:3000/api/register";
-  public static final API_LOGIN: String = "http://localhost:3000/api/login";
+  public static final API_REGISTER: String = "https://oppschrifter.herokuapp.com/api/register";
+  public static final API_LOGIN: String = "https://oppschrifter.herokuapp.com/api/login";
   //http://localhost:3000/api/register
 }
