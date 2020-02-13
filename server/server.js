ab558adb942fa0a61eaeb3673062df05db89318e
Ludvig Ellevold <21200123+ljellevo@users.noreply.github.com>
2020-02-13 19:27:10 +0100

    Printing port

--- server/server.js
+++ server/server.js
@@ -24,6 +24,6 @@ app.get('/*', function (req, res) {
 });
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Listening on port 3000!")
+  console.log("Listening on port: " + process.env.PORT || 3000)
 });
 
